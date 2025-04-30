617b273536a0eb3667c2c92e953bdda91b99232c
SSablin <sablinserg2007@yandex.ru>
2025-05-01 00:11:50 +0300

    30.04

---
+++ School/Задание 14.py
@@ -0,0 +1,17 @@
+# ЕГКР 19.04 В1
+"""def chi(a, i):
+    s = ''
+    while a > 0:
+        s = str(a % i) + s
+        a //= i
+    return s
+
+
+mn = float('inf')
+for x in range(0, 2000):
+    a = 4 ** 210 + 4 ** 110 - x
+    a_4 = chi(a, 4)
+    if a_4.count('0') <= mn:
+        mn = a_4.count('0')
+        print(x, mn)  # 1707"""
+
