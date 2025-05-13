c568c6d11dd9e6544b67edea0db2886e95af3072
SSablin <sablinserg2007@yandex.ru>
2025-05-13 19:44:29 +0300

    13.05

--- School/Variants/Вариант Шастинa #11.py
+++ School/Variants/Вариант Шастинa #11.py
@@ -1,3 +1,35 @@
+# 6
+"""from turtle import *
+tracer(0)
+screensize(5000, 5000)
+c = 25
+lt(90)
+
+fd(30 * c), lt(60), fd(24 * c), rt(240)
+fd(54 * c), lt(120), fd(24 * c), lt(60)
+up()
+fd(30 * c), rt(90), fd(20 * c), lt(90)
+down()
+for i in range(17):
+    fd(6 * c), lt(90), fd(80 * c), lt(90)
+
+up()
+for x in range(-100, 100):
+    for y in range(-100, 100):
+        setpos(x * c, y * c)
+        dot(3, 'green')
+done()
+print(int(6 + 6*3**0.5 + 12 + 12*3**0.5))  # 49"""
+
+# 7
+"""data1 = 1000 * 1024 * 1024 * 8
+t1 = data1 / 2**23
+data2 = 0.6 * data1
+t3 = data2 / 2**20
+t2 = 100 * 60 - t1 - t3
+print(t2)  # 200"""
+
+
 # 9
 """for s in open('files/9_шастин10.txt'):
     a = list(map(int, s.split()))
@@ -11,14 +43,22 @@
     if len(set(a)) == len(a) and sum(chet)**2 > sum(nchet)**3:
         print(a, sum(a))  # 246"""
 
+# 11
+"""from math import ceil, log2
+N = 18 + 10
+i = ceil(log2(N))
+s = ceil(i * 12 / 8)
+data = 28 - s
+print(data)  # 20"""
+
 # 13
 """from ipaddress import *
 net = ip_network('98.71.254.171/255.248.0.0', 0)
 for ip in net.hosts():
     b = f'{ip:b}'
     if b.count('1') % 5 == 0:
-        print(ip)"""
-# 9871255248
+        print(ip)
+# 9871255248"""
 
 # 23
 """def f(x, end):
@@ -70,7 +110,6 @@ def F(x):
     for i in range(7, x, 10):
         if x % i == 0 and i % 10 == 7 and simple(i):
             s.add(i)
-
     if len(s) == 0:
         return 0
     return sum(s) // len(s)
